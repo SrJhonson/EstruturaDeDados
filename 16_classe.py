fa8f03c02ffdbada54c00b4c871f689bff12ca75
Jhonatan Dias Farias <diasjhonatan24@gmail.com>
2024-04-15 19:15:43 -0300

    Aula 15/04

--- 16_classe.py
+++ 16_classe.py
@@ -7,6 +7,8 @@ fazer" (algoritmos). Apesar dessas variações, todos os objetos
 criados a partir de uma mesma classe terão sempre algumas
 características comuns impostas por esta.
 """
+from math import pi
+
 class FormaGeometrica:
     """
     Por convenção, nomes de classes seguem o formato PascalCase
@@ -24,15 +26,98 @@ class FormaGeometrica:
 
     def __init__(self, base, altura, tipo):
         """ Método construtor """
+        self.set_base(base)
+        self.set_altura(altura)
+        self.set_tipo(tipo)
 
-        # Validações dos dados iniciais
-        if type(base) not in [int, float]:
-            raise Exception("ERRO: o parâmetro 'base' deve ser numérico.")
-        if type(altura) not in [int, float]:
-            raise Exception("ERRO: o parâmetro 'altura' deve ser numérico.")
-        if tipo not in ["R", "T", "E"]:
+    def set_base(self, val):
+        """
+        Método setter para o atributo __base
+        """
+        if type(val) not in [int, float] or val <= 0:
+            raise Exception("ERRO: o parâmetro 'base' deve ser numérico e maior que zero.")
+        self.__base = val
+
+    def set_altura(self, val):
+        """
+        Método setter para o atributo __altura
+        """
+        if type(val) not in [int, float] or val <= 0:
+            raise Exception("ERRO: o parâmetro 'altura' deve ser numérico e maior que zero.")
+        self.__altura = val
+
+    def set_tipo(self, val):
+        """
+        Método setter para o atributo __tipo
+        """
+        if val not in ["R", "T", "E"]:
             raise Exception("ERRO: o parâmetro 'tipo' deve ter um dos seguintes valores: 'R', 'T' ou 'E'.")
+        self.__tipo = val
 
-        self.base = base
-        self.altura = altura
-        self.tipo = tipo
+    def get_base(self):
+        """
+        Método getter para o atributo __base
+        """
+        return self.__base
+    
+    def get_altura(self):
+        """
+        Método getter para o atributo __altura
+        """
+        return self.__altura
+    
+    def get_tipo(self):
+        """
+        Método getter para o atributo __tipo
+        """
+        return self.__tipo
+    
+    def calc_area(self):
+        """
+        Método que calcula a área da forma, com base nos valores de
+        __base, __altura e __tipo
+        """
+        if self.__tipo == "R":      # Retângulo
+            return self.__base * self.__altura
+        if self.__tipo == "T":      # Triângulo
+            return self.__base * self.__altura / 2
+        else:                       # Elipse/círculo
+            return (self.__base / 2) * (self.__altura / 2) * pi
+
+    def __str__(self):
+        """
+        Retorna uma representação dos valores armazenados dentro do objeto
+        como uma string, para que possamos visualizá-los
+        """
+        return f"[OBJETO] base: {self.__base}, altura: {self.__altura}, tipo: {self.__tipo}"
+
+#############################################################################
+        
+forma1 = FormaGeometrica(10, 13, "R")
+forma2 = FormaGeometrica(7.5, 8.2, "T")
+
+#forma1.base = "batata"
+#forma1.set_base("batata")
+forma1.set_base(12)
+#forma1.altura = -3
+forma1.set_altura(3)
+#forma1.tipo = "K"
+forma1.set_tipo("E")
+
+print(forma1)
+print(forma2)
+
+print('-' * 60)
+
+#print(f"Base de forma1: {forma1.__base}")
+print(f"Base de forma1: {forma1.get_base()}")
+print(f"Altura de forma1: {forma1.get_altura()}")
+print(f"Tipo de forma1: {forma1.get_tipo()}")
+print(f"Área de forma1: {forma1.calc_area()}")
+
+print('-' * 60)
+
+print(f"Base de forma2: {forma2.get_base()}")
+print(f"Altura de forma2: {forma2.get_altura()}")
+print(f"Tipo de forma2: {forma2.get_tipo()}")
+print(f"Área de forma2: {forma2.calc_area()}")
