55d07714b34f32d1390e3647aceba98582aec1ae
Jhonatan Dias Farias <diasjhonatan24@gmail.com>
2024-04-22 21:11:11 -0300

    Aula 20/21

--- 19_pilha3.py
+++ 19_pilha3.py
@@ -6,7 +6,7 @@ from lib.stack import Stack
 
 pilha = Stack()
 
-expr = "(2 * ((3 + 4) - (5 / 3) (+ 1) - 2"
+expr = "(2 * (3 + 4) - (5 / 3) + 1) - 2"
 
 
 for pos in range(len(expr)):
@@ -17,16 +17,7 @@ for pos in range(len(expr)):
     # 2ª PARTE: ao encontrar um caractere de fecha parêntese,
     #           desempilha
     elif expr[pos] == ")":
-        #A pilha não pode estar vazia quando for encontrado um fecha parentese
-        if pilha.is_empty():
-            print(f"Erro: parentese fechado na posição {pos} não tem o abre correspondente")
-        else:
-            pos_emp = pilha.pop()
-            print(f"Parêntese aberto na posição {pos_emp} foi fechado na posição {pos}.")
+        pos_emp = pilha.pop()
+        print(f"Parêntese aberto na posição {pos_emp} foi fechado na posição {pos}.")
 
 print(pilha)
-
-# Processa eventuais sobras na pilha após o termino da análise da expressão
-while not pilha.is_empty():
-    pos = pilha.pop()
-    print(f"Erro: parentese aberto na posição {pos} não tem o fecha correspondente")
